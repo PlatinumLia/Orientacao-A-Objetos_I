780f89453dd292b8b691951a7b9498b66322a196
Lia <134401063+PlatinumLia@users.noreply.github.com>
2025-10-07 22:25:11 -0300

    Atualizar o Vendas.java

--- Aula 09 - Arquivos(2)/Vendas.java
+++ Aula 09 - Arquivos(2)/Vendas.java
@@ -16,6 +16,7 @@ public class Vendas{
         int i = 0;
         float soma = 0;
         float somaVista = 0;
+        float somaPrazo = 0;
 
         while((venda = arqLeitura.readLine()) != null){
             //valor total das vendas
@@ -23,8 +24,10 @@ public class Vendas{
             soma += Float.parseFloat(vetVenda[2].replace(",", "."));
             
             //valor total das vendas à vista
-            if(vetVenda[3] == String.valueOf('V')){
+            if(vetVenda[3].equals("V")){
                 somaVista += Float.parseFloat(vetVenda[2].replace(",", "."));
+            }else{
+               somaPrazo += Float.parseFloat(vetVenda[2].replace(",", "."));
             }
             i++;//quantidade de vendas realizadas
         }
@@ -32,5 +35,6 @@ public class Vendas{
         System.out.println("Quantidade total de vendas: " + i);
         System.out.println("Total de todas as vendas: " + soma);
         System.out.println("Total de vendas à vista: " + somaVista);
+        System.out.println("Total de vendas à prazo: " + somaPrazo);
     }    
 }
