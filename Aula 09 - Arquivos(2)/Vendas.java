537b6c8a5e5adda86326a99f88ab165620d96b32
Lia <134401063+PlatinumLia@users.noreply.github.com>
2025-10-07 22:02:53 -0300

    Add files via upload

---
+++ Aula 09 - Arquivos(2)/Vendas.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+// 4 -Valor total das vendas à prazo
+
+public class Vendas{
+    @SuppressWarnings("StringEquality")
+    public static void main(String[] args) throws Exception{
+        String nomeArq = "vendas.csv";
+        BufferedReader arqLeitura = new BufferedReader(new FileReader(nomeArq));
+
+        arqLeitura = new BufferedReader(new FileReader(nomeArq));
+        
+        String venda;
+        venda = arqLeitura.readLine();
+        int i = 0;
+        float soma = 0;
+        float somaVista = 0;
+
+        while((venda = arqLeitura.readLine()) != null){
+            //valor total das vendas
+            String[] vetVenda = venda.split(";");
+            soma += Float.parseFloat(vetVenda[2].replace(",", "."));
+            
+            //valor total das vendas à vista
+            if(vetVenda[3] == String.valueOf('V')){
+                somaVista += Float.parseFloat(vetVenda[2].replace(",", "."));
+            }
+            i++;//quantidade de vendas realizadas
+        }
+        arqLeitura.close();
+        System.out.println("Quantidade total de vendas: " + i);
+        System.out.println("Total de todas as vendas: " + soma);
+        System.out.println("Total de vendas à vista: " + somaVista);
+    }    
+}
